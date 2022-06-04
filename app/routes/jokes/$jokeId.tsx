6695b53aa5b183c5034e49d1b3034b24a5c9471b
Ben Bagley <hello@benbagley.dev>
2022-06-04 17:15:21 +0100

    added ramdom joke and finished jokeId page

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,32 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { joke } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+
+  if (!joke) throw Error("Joke not found");
+
+  const data: LoaderData = { joke };
+
+  return json(data);
+};
+
 export default function Joke() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <>
       <p>Here is your joke...</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? {""}
-        There really good at it.
-      </p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name}</Link>
     </>
   );
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,34 @@
+import { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+import { joke } from "@prisma/client";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random() * count);
+
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomNumber,
+  });
+
+  const data: LoaderData = { randomJoke };
+
+  return json(data);
+};
+
 export default function JokesIndex() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <>
       <p>Here is a joke</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </>
   );
 }
