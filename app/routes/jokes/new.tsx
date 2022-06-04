3c1c48dde33af845b1ad6c473203c8a7e04f5ac2
Ben Bagley <hello@benbagley.dev>
2022-06-04 17:56:07 +0100

    added server side validation to new form

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,25 +1,64 @@
 import type { ActionFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+
+function validateJokesName(name: String) {
+  if (name.length < 3) {
+    return "Your jokes name is too short";
+  }
+}
+
+function validateJokesContent(content: String) {
+  if (content.length < 10) {
+    return "Your jokes content is too short";
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
-  // we do this type check to be extra sure and to make TypeScript happy
-  // we'll explore validation next!
+
   if (typeof name !== "string" || typeof content !== "string") {
-    throw new Error(`Form not submitted correctly.`);
+    return badRequest({
+      formError: "Form is not submitted correctly",
+    });
   }
 
+  const fieldErrors = {
+    name: validateJokesName(name),
+    content: validateJokesContent(content),
+  };
+
   const fields = { name, content };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
 
   const joke = await db.joke.create({ data: fields });
   return redirect(`/jokes/${joke.id}`);
 };
 
 export default function NewJokeRoute() {
+  const actionData = useActionData<ActionData>();
+
   return (
     <>
       <h1>Add your own joke here.</h1>
@@ -27,12 +66,45 @@ export default function NewJokeRoute() {
       <form method="post">
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+          {""}
+          <input
+            type="text"
+            name="name"
+            defaultValue={actionData?.fields?.name}
+            aria-invalid={Boolean(actionData?.fields?.name) || undefined}
+            aria-errormessage={
+              actionData?.fieldErrors?.name ? "name-error" : undefined
+            }
+          />
+
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
 
         <div>
           <label htmlFor="content">Content</label>
-          <textarea name="content" />
+          {""}
+          <textarea
+            name="content"
+            defaultValue={actionData?.fields?.content}
+            aria-invalid={Boolean(actionData?.fields?.content) || undefined}
+            aria-errormessage={
+              actionData?.fieldErrors?.content ? "content-error" : undefined
+            }
+          />
+
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              role="alert"
+              id="content-error"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
         </div>
 
         <div>
